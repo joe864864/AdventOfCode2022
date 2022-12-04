eb6340c527ea7526314be2a2f56bbf6760c27a52
joe864864 <joe864864@gmail.com>
2022-12-04 12:46:22 -0500

    Day 4 Init

--- Day 4/Day 4 - Pt 1.ps1
+++ Day 4/Day 4 - Pt 1.ps1
@@ -3,5 +3,29 @@
 #Date: 12/4/2022
 
 #Read through all the data and computer total calorie by elf
-$data = Get-Content "Day 3 - Input.txt"
+$data = Get-Content "Day 4 - Input.txt"
 
+$pairs = @()
+
+foreach($line in $data){
+    $temp = $line.Split(",").Split("-")
+    foreach($num in $temp){
+        $pairs += [int]$num
+    }
+    #Write-Host($pairs)
+}
+
+$numDupes = 0
+
+#Check for any complete overlaps
+for($i=0;$i -lt $pairs.Length;$i+=4){
+    Write-Host([String]$pairs[$i] + "-" + [String]$pairs[$i+1] + "," + [String]$pairs[$i+2] + "-" + [String]$pairs[$i+3])
+    if($pairs[$i] -ge $pairs[$i+2] -and $pairs[$i+1] -le $pairs[$i+3]){
+        $numDupes++
+    }
+    elseif($pairs[$i+2] -ge $pairs[$i] -and $pairs[$i+3] -le $pairs[$i+1]){
+        $numDupes++
+    }
+}
+
+Write-Host("Total duplicates: " + $numDupes)
--- Day 4/Day 4 - Pt 2.ps1
+++ Day 4/Day 4 - Pt 2.ps1
@@ -3,5 +3,31 @@
 #Date: 12/4/2022
 
 #Read through all the data and computer total calorie by elf
-$data = Get-Content "Day 3 - Input.txt"
+$data = Get-Content "Day 4 - Input.txt"
 
+$pairs = @()
+
+foreach($line in $data){
+    $temp = $line.Split(",").Split("-")
+    foreach($num in $temp){
+        $pairs += [int]$num
+    }
+    #Write-Host($pairs)
+}
+
+$numDupes = 0
+
+#Check for any overlap
+for($i=0;$i -lt $pairs.Length;$i+=4){
+    Write-Host([String]$pairs[$i] + "-" + [String]$pairs[$i+1] + "," + [String]$pairs[$i+2] + "-" + [String]$pairs[$i+3])
+    if($pairs[$i] -ge $pairs[$i+2] -and $pairs[$i] -le $pairs[$i+3]){
+        $numDupes++
+        Write-Host("1")
+    }
+    elseif($pairs[$i+1] -ge $pairs[$i+2] -and $pairs[$i] -le $pairs[$i+3]){
+        $numDupes++
+        Write-Host("2")
+    }
+}
+
+Write-Host("Total duplicates: " + $numDupes)
