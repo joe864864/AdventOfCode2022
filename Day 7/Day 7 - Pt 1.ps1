4b863e1f4d1330650a685774aef8d03b9891477a
joe864864 <joe864864@gmail.com>
2022-12-10 21:13:04 -0500

    Day 7 Init

--- Day 7/Day 7 - Pt 1.ps1
+++ Day 7/Day 7 - Pt 1.ps1
@@ -1,26 +1,57 @@
 #Advent of Code 2022
-#Author: Joseph Scott
-#Date: 12/9/2022
+#Date: 12/10/2022
 
 #Read through all the data and computer total calorie by elf
-$data = Get-Content "Day 7 - Test Input.txt"
+$data = Get-Content "Day 7 - Input.txt"
 
-$table = @{}
-
-for($i=0;$i-lt$data.Length;$i++){
-    $line = [string]$data[$i]
-    if($line[0]-eq"$"){
-        if($line[2]-eq"c" -and $line[3]-eq"d"){
-            Write-Host([string]$i + ":cd")
-        }
-        elseif($line[2]-eq"l" -and $line[3]-eq"s"){
-            Write-Host([string]$i + ":ls")
-        }
-    }
-    elseif($line[0]-eq"d" -and $line[1]-eq"i" -and $line[2]-eq"r"){
-        Write-Host([string]$i + ":dir")
-    }
-    else{
-        Write-Host([string]$i + ":file")
-    }
+#Helper that returns the parent path of a given folder
+function UpDir {
+    param([string]$dir)
+    $dir = $dir.Substring(0,$dir.LastIndexOf('/'))
+    return ($dir -eq '') ? '/' : $dir
 }
+
+#Parse the file structure into a hashtable where each absolute
+#Folder path is a key and its value is the total size of its children
+$fileSys = @{}
+switch -Regex ($data) {
+
+    #Change directory
+    '\$ cd (\S+)' {
+        if ($matches[1] -eq '/') {
+            $curDir = '/'
+        }
+        elseif ($matches[1] -eq '..') {
+            $curDir = UpDir $curDir
+        }
+        else {
+            $curDir += ($curDir -eq '/') ? $matches[1] : "/$($matches[1])"
+        }
+
+        #Add the current dir to the hashtable if it doesn't exist
+        if ($null -eq $fileSys[$curDir]) {
+            $fileSys[$curDir] = 0
+        }
+    }
+
+    #File
+    '(\d+) (\S+)' {
+
+        #Add the file size to the current directory
+        $fileSys[$curDir] += [int]$matches[1]
+
+        #Add it to all of the parent directories as well
+        $tempDir = $curDir
+        while ($tempDir -ne '/') {
+            $tempDir = UpDir $tempDir
+            $fileSys[$tempDir] += [int]$matches[1]
+        }
+    }
+
+}
+
+Write-Host("Total size: ")
+$fileSys.GetEnumerator()
+| Where-Object { $_.Value -le 100000 }
+| Measure-Object -Sum Value
+| Select-Object -Expand sum
--- Day 7/Day 7 - Pt 2.ps1
+++ Day 7/Day 7 - Pt 2.ps1
@@ -1,7 +1,59 @@
 #Advent of Code 2022
-#Author: Joseph Scott
-#Date: 12/9/2022
+#Date: 12/10/2022
 
 #Read through all the data and computer total calorie by elf
-$data = Get-Content "Day 7 - Test Input.txt"
+$data = Get-Content "Day 7 - Input.txt"
 
+#Helper that returns the parent path of a given folder
+function UpDir {
+    param([string]$dir)
+    $dir = $dir.Substring(0,$dir.LastIndexOf('/'))
+    return ($dir -eq '') ? '/' : $dir
+}
+
+#Parse the file structure into a hashtable where each absolute
+#Folder path is a key and its value is the total size of its children
+$fileSys = @{}
+switch -Regex ($data) {
+
+    #Change directory
+    '\$ cd (\S+)' {
+        if ($matches[1] -eq '/') {
+            $curDir = '/'
+        }
+        elseif ($matches[1] -eq '..') {
+            $curDir = UpDir $curDir
+        }
+        else {
+            $curDir += ($curDir -eq '/') ? $matches[1] : "/$($matches[1])"
+        }
+
+        #Add the current dir to the hashtable if it doesn't exist
+        if ($null -eq $fileSys[$curDir]) {
+            $fileSys[$curDir] = 0
+        }
+    }
+
+    #File
+    '(\d+) (\S+)' {
+
+        #Add the file size to the current directory
+        $fileSys[$curDir] += [int]$matches[1]
+
+        #Add it to all of the parent directories as well
+        $tempDir = $curDir
+        while ($tempDir -ne '/') {
+            $tempDir = UpDir $tempDir
+            $fileSys[$tempDir] += [int]$matches[1]
+        }
+    }
+
+}
+
+Write-Host("Dir size: ")
+$usedSpace = $fileSys['/']
+$needed = 30000000 - (70000000 - $usedSpace)
+$fileSys.GetEnumerator()
+| Where-Object { $_.Value -ge $needed }
+| Sort-Object Value
+| Select-Object -First 1 -Expand Value
